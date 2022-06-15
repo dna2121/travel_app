be8388c5f3b86814896080a9a9956d76b84e60d0
dna2121 <dnatayawibisono@gmail.com>
2022-06-15 10:51:22 +0700

    places item

--- lib/widgets/places_item.dart
+++ lib/widgets/places_item.dart
@@ -8,55 +8,58 @@ class PlacesItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 205,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Color.fromARGB(255, 227, 227, 227),
-        boxShadow: const [
-          BoxShadow(
-            color: Color.fromARGB(51, 104, 142, 181),
-            blurRadius: 20,
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.network("https://picsum.photos/200/300",
-                  height: 183, width: 177, fit: BoxFit.cover),
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        width: 205,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Color.fromARGB(255, 227, 227, 227),
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(51, 104, 142, 181),
+              blurRadius: 20,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(24, 0, 15, 4),
-            child: Text(
-              "Mountain",
-              style: GoogleFonts.poppins(fontSize: 20),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network("https://picsum.photos/200/300",
+                    height: 183, width: 177, fit: BoxFit.cover),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(24, 0, 15, 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Nepal",
-                  style: GoogleFonts.poppins(
-                      fontSize: 18, fontWeight: FontWeight.w300),
-                ),
-                Text(
-                  "⭐ 4.5",
-                  style: GoogleFonts.poppins(
-                      fontSize: 18, fontWeight: FontWeight.w300),
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(24, 0, 15, 4),
+              child: Text(
+                "Mountain",
+                style: GoogleFonts.poppins(fontSize: 20),
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(24, 0, 15, 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Nepal",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18, fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    "⭐ 4.5",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18, fontWeight: FontWeight.w300),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
